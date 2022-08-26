3601f2200c57284fe1ade45f4ecf4162f4e0b8cc
harsh-vora9 <83445984+harsh-vora9@users.noreply.github.com>
2022-08-27 01:32:12 +0530

    Create empty components and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
-  return <div>App</div>;
-};
+import Navbar from './components/layout/navbar/Navbar';
+import Feed from './components/feed/Feed';
+import SearchFeed from './components/feed/SearchFeed';
+import VideoDetail from './components/video/VideoDetail';
+import ChannelDetail from './components/channel/ChannelDetail';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
