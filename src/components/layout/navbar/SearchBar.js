6139b7231951b4a4325295821f9563aec3375b34
harsh-vora9 <83445984+harsh-vora9@users.noreply.github.com>
2022-08-27 02:32:13 +0530

    Navbar HTML

---
+++ src/components/layout/navbar/SearchBar.js
@@ -0,0 +1,38 @@
+import { Search } from '@mui/icons-material';
+import { IconButton, Paper } from '@mui/material';
+
+import classes from './SearchBar.module.css';
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className={classes.searchBar}
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton
+        type="submit"
+        sx={{
+          p: '10px',
+          color: 'red',
+        }}
+      >
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
